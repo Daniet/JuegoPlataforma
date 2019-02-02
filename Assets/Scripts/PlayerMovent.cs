6ce54a9463ce805fedd926ca4565c38361a3af1a
Daniel Andres <dapinto7@misena.edu.co>
2019-02-02 17:54:49 -0500

    inicio de juego de plataforma

---
+++ Assets/Scripts/PlayerMovent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovent : MonoBehaviour {
+
+	public Rigidbody2D playerRb;
+	public float speed = 4f;
+	public float jumpSpeed = 500f;
+	bool isGrounded = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.velocity.y);
+		if(Input.GetKeyDown(KeyCode.Space)){
+			playerRb.AddForce(Vector2.up * speed);
+		}
+		if(isGrounded){
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				playerRb.AddForce (Vector2.up * jumpSpeed);
+				isGrounded = false;
+			}
+		}
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D collision){
+		if(collision.gameObject.CompareTag("Ground")){
+			isGrounded = true;
+		}
+	}
+}
