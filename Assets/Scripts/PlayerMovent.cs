badd996ec557e79cc23be45de7a559441e2be5c3
Daniel Andres <dapinto7@misena.edu.co>
2019-02-06 23:06:09 -0500

    ajuste de animacion de enemigo

--- Assets/Scripts/PlayerMovent.cs
+++ Assets/Scripts/PlayerMovent.cs
@@ -9,6 +9,7 @@ public class PlayerMovent : MonoBehaviour {
 	public float jumpSpeed = 500f;
 	bool isGrounded = true;
 	// Use this for initialization
+	public Animator playerAnim;
 	void Start () {
 		
 	}
@@ -16,13 +17,26 @@ public class PlayerMovent : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.velocity.y);
+
+		if(Input.GetAxis("Horizontal") == 0){
+			playerAnim.SetBool("isWalking", false);
+		}else if(Input.GetAxis("Horizontal") < 0){
+			playerAnim.SetBool("isWalking", true);
+			GetComponent<SpriteRenderer>().flipX = true;
+		}else if(Input.GetAxis("Horizontal") > 0){
+			playerAnim.SetBool("isWalking", true);
+			GetComponent<SpriteRenderer>().flipX = false;
+		}
+
 		if(Input.GetKeyDown(KeyCode.Space)){
 			playerRb.AddForce(Vector2.up * speed);
 		}
+
 		if(isGrounded){
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				playerRb.AddForce (Vector2.up * jumpSpeed);
 				isGrounded = false;
+				playerAnim.SetTrigger("Jump");
 			}
 		}
 	}
