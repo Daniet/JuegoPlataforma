ec73b725fc0f04c99043e3a57fd0c5b47d384b0b
Daniel Andres <dapinto7@misena.edu.co>
2019-02-09 03:55:29 -0500

    coleccionable

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log ("Colision");
+		if(col.tag == "Player"){
+			gameObject.SetActive(false);
+		}
+	}
+}
